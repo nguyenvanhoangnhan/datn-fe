ec7fbaaa006314a978ed02626f3a2805bd6b2f81
Nhân Nguyễn <nguyen.vh.nhan@gmail.com>
2024-06-19 00:39:38 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ongakool',
+  webDir: 'dist'
+};
+
+export default config;
